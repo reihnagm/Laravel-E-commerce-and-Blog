b618f68a95e92d053598038dd9b579d9176bd751
Reihan Agam <reihanagam7@gmail.com>
2018-09-30 22:49:56 +0700

    update

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ $url = parse_url(getenv("DATABASE_URL"));
 
 $host = $url["host"]?? null;
 $username = $url["user"]??null;
-$password = $url["password"]??null;
+$password = $url["pass"]??null;
 $database = substr($url["path"], 1)??null;
 
 return [
